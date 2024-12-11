e2c82633f01c6cdee407ef827466d163c1da965f
Albertinoco <118600597+Albertinoco@users.noreply.github.com>
2024-12-12 00:05:42 +0100

    JogoOnline

---
+++ game.js
@@ -0,0 +1,17 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+var nextSequence = function () {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColor);
+  console.log(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+};
